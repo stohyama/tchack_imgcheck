65d12fcd575b489a0f7c5b873b170d51f8564b1b
yutaka0m <yutaka.miyamae@askul.com>
2019-05-30 15:25:40 +0000

    add TypeHints and Hints

--- classes/image_check.py
+++ classes/image_check.py
@@ -3,12 +3,22 @@ import base64
 import json
 import os
 import sys
-from classes.scraping import return_scraping_html_array
+import scraping
+from image_information import ImageInformation
+
+def url_to_image(url: str) -> bytes:
+    try:
+        img = requests.get(url)
+    except requests.exceptions.ConnectionError:
+        print('NewtorkError')
+    except requests.exceptions.TooManyRedirects:
+        print('TooManyRedirects')
+    except requests.exceptions.HTTPError:
+        print('BadResponse')
+    return img.content
 
 
-def img_to_base64(filepath):
-    with open(filepath, 'rb') as img:
-        img_byte = img.read()
+def img_to_base64(image: bytes) -> bytes:
     return base64.encodebytes(img_byte)
 
 
@@ -39,38 +49,41 @@ def get_json_from_googlecloud_vision_api(image_base64):
     return res.json()
 
 
-def is_rule_001():  # 全L1画像共通加工ルール    背景色白の基本パターン    画像サイズ    縦：600px 横：600px
-    # def is_rule_001(vision_api_json) -> vision_apiのjsonをつかう場合は、こう書く
+def is_rule_001(image_info: ImageInformation) -> bool:  # 全L1画像共通加工ルール    背景色白の基本パターン    画像サイズ    縦：600px 横：600px
     return False
 
 
-def is_rule_002():  # 全L1画像共通加工ルール    背景色白の基本パターン    容量    50kb以下
+def is_rule_002(image_info: ImageInformation) -> bool:  # 全L1画像共通加工ルール    背景色白の基本パターン    容量    50kb以下
     return False
 
 
-def is_rule_003():  # 全L1画像共通加工ルール 背景色白の基本パターン 解像度 72dpi
+def is_rule_003(image_info: ImageInformation) -> bool:  # 全L1画像共通加工ルール 背景色白の基本パターン 解像度 72dpi
     return False
 
 
-def is_rule_004():
+def is_rule_004(image_info: ImageInformation) -> bool:
     return False
 
 # 以下 is_rule_*** をつくっていってください
+
+def check_all_rules(image_info: ImageInformation) -> list:
+    #すべてのルールを呼んできて、結果を配列に入れる
+    return [['画像サイズは600*600',True],['アダルトではない',False]]
 
 
 def main():
     lpm_url = "https://lohaco.jp/store/irisplaza/ksearch/?categoryLl=55"
     max_page = 100
 
-    scraping_html_array = return_scraping_html_array(lpm_url, max_page)
-    img_base64 = img_to_base64('./sample.jpg')
-    googlecloud_vision_json = get_json_from_googlecloud_vision_api(img_base64)
-
-    print('is_rule_001', is_rule_001())
-
-    print('is_rule_002', is_rule_002())
-
-    print('is_rule_003', is_rule_003())
+    scraping_html_array = scraping.return_scraping_html_array(lpm_url, max_page)
+    
+    #ここからfor文
+        #img = url_to_image(hogehoge)
+        #img_base64 = img_to_base64(img)
+        #googlecloud_vision_json = get_json_from_googlecloud_vision_api(img_base64)
+        #ImageInformationのインスタンスをつくるimage_info = ?
+        #results = check_all_rules(image_info)
+        #print_scv(image_info)
 
 
 if __name__ == '__main__':
